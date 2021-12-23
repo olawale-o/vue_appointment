202ae2cf80e6a01647dfca6e58807984f432ad59
olawale-o <omoogunolawale@gmail.com>
2021-12-23 13:43:17 +0100

    Use router object

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './assets/index.css'
 import App from './App.vue'
+import router from './routes';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
