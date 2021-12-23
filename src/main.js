ffc4b0978f02053e24f31bda5ea37efbcd07706f
olawale-o <omoogunolawale@gmail.com>
2021-12-23 21:49:30 +0100

    Use store

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import './assets/index.css'
 import App from './App.vue'
 import router from './routes';
+import store from './redux';
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount('#app');
