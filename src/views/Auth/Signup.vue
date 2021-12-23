0be5250026bdaa64c7cbc9f69e58bb7a49e0c611
olawale-o <omoogunolawale@gmail.com>
2021-12-23 12:11:24 +0100

    Rename component to view

---
+++ src/views/Auth/Signup.vue
@@ -0,0 +1,55 @@
+<template>
+  <div className="login__container">
+    <div className="form">
+      <form>
+        <h3>Book an appointment</h3>
+        <div className={style.field}>
+          <input
+            type="text"
+            placeholder="Full name"
+            className="input"
+            required
+          />
+        </div>
+        <div className={style.field}>
+          <input
+            type="text"
+            placeholder="Email"
+            className="input"
+            required
+          />
+        </div>
+        <div className={style.field}>
+          <input
+            type="password"
+            placeholder="Password"
+            className="input"
+            required
+          />
+        </div>
+        <div className={style.field}>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            className="input"
+            required
+          />
+        </div>
+        <div className="actions">
+          <input type="submit" value="Create" className="btn auth__btn" />}
+        </div>
+        <a href="/login" className="link">Already have an account?</a>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
