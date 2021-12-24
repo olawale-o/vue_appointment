24619712cf52ec6ca28dfcdae916f0321a1fc6d7
olawale-o <omoogunolawale@gmail.com>
2021-12-24 15:13:55 +0100

    Create Private view

---
+++ src/views/Private/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div className="home">
+    <Nav />
+    <div className="child">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Nav from '@/components/Nav.vue';
+  export default {
+    name: 'Private',
+    components: { Nav },
+    setup() {
+  
+    },
+  }
+</script>
+
+<style scoped>
+  .home {
+    min-height: 90vh;
+    width: 100%;
+    display: flex;
+  }
+
+  .child {
+    position: relative;
+    width: 100%;
+  }
+
+  @media screen and (min-width: 992px) {
+    .child {
+      margin-left: auto;
+      width: 75vw;
+    }
+  }
+
+</style>
