887b3b71c809d4f7b89d3291a3447ee73fcc8f87
olawale-o <omoogunolawale@gmail.com>
2021-12-25 12:54:24 +0100

    Add doctorModule to store

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,13 @@
 import { createStore } from 'vuex';
 import authModule from './auth';
+import doctorModule from './doctor';
 import rootStore from './root';
 
 const store = createStore({
   ...rootStore,
   modules: {
    auth: authModule,
+   doctor: doctorModule,
   },
 });
 export default store;
