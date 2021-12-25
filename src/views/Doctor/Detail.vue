71226e99733d85e1a04eaa3d383a062dd2915860
olawale-o <omoogunolawale@gmail.com>
2021-12-25 20:27:31 +0100

    Create doctor detail component

---
+++ src/views/Doctor/Detail.vue
@@ -0,0 +1,16 @@
+<template>
+ <div>Doctor Details</div>
+</template>
+
+<script>
+  export default {
+    name: 'DoctorDetail',
+    setup() {
+      console.log('DoctorDetail');      
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
