9d270e37d95ed4febd9402ccdc11a99824b9eaed
olawale-o <omoogunolawale@gmail.com>
2021-12-26 07:22:04 +0100

    Display all doctors to delete from

---
+++ src/components/DoctorItemCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="table__row">
+    <div class="info">
+      <img
+        :src="`${BASE_URI}${doctor.picture}`"
+        class="avatar"
+        alt="appointment"
+      />
+      <span class="fs-3">{{doctor.fullname}}</span>
+    </div>
+    <div class="city">
+      <span class="fs-3 bold xs">City: </span>
+      <span class="fs-3">{{doctor.city}}</span>
+    </div>
+    <div class="specialty">
+      <span class="fs-3 bold xs">Specialty: </span>
+      <span class="fs-3">{{doctor.specialty}}</span>
+    </div>
+    <span class="fs-6">
+      <button
+        type="button"
+        class="btn"
+      >
+        <i class="bx bx-trash-alt" />
+      </button>
+    </span>
+  </div>
+  <div class="divider" />
+</template>
+
+<script>
+  import BASE_URI from '@/constants/url';
+  export default {
+    name: 'DoctorItemCard',
+    props: {
+      doctor: {
+        type: Object,
+        required: true,
+      },
+    },
+    setup() {
+      return { BASE_URI };
+    },
+  }
+</script>
+
+<style scoped>
+  .avatar {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    object-fit: 'cover';
+  }
+</style>
---
+++ src/views/Doctor/All.vue
@@ -0,0 +1,35 @@
+<template>
+  <div className="Appointments" style="background: 'whitesmoke', min-height: '700px', padding-top: '10rem'">
+    <div className="table__container">
+      <div className="loading__indicator" v-if="loading" />
+      <template v-if="doctors.length > 0">
+        <h1 className="heading1">My Doctors</h1>
+        <DoctorList :doctors="doctors" />
+      </template>
+      <div style="font-size: '3rem', text-align: 'center'" v-if="doctors.length === 0">You dont have doctors</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { computed } from 'vue';
+  import { useStore } from 'vuex';
+  import DoctorList from '@/components/DoctorList';
+  export default {
+    name: 'RemoveDoctor',
+    components: {
+      DoctorList,
+    },
+    setup() {
+      const store = useStore();
+      return {
+        doctors: computed(() => store.getters['doctor/doctors']),
+        loading: computed(() => store.getters.loading),
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
