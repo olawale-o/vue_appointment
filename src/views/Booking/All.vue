9a7e5846cce42bd762328facdce278722a298935
olawale-o <omoogunolawale@gmail.com>
2021-12-28 20:36:24 +0100

    User can delete and add appointment

---
+++ src/views/Booking/All.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="appointments">
+    <div class="table__container">
+      <div class="loading__indicator" v-if="loading" />
+      <template v-if="bookings.length > 0">
+        <h1 class="heading1">My Appointments</h1>
+        <AppointmentList :appointments="bookings" />
+      </template>
+      <div v-if="bookings.length === 0" class="empty">No appointments yet</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { computed } from 'vue';
+  import { useStore } from 'vuex';
+  import AppointmentList from '@/components/AppointmentList.vue';
+  export default {
+    name: 'AllAppointments',
+    components: {
+      AppointmentList,
+    },
+    setup() {
+      const store = useStore();
+      return {
+        bookings: computed(() => store.getters['appointment/appointments']),
+        loading: computed(() => store.getters.loading),
+      }
+    },
+  }
+</script>
+
+<style scoped>
+</style>
