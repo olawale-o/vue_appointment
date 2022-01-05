ef883c7af8c1f54cef716283187454ca72272eec
olawale-o <omoogunolawale@gmail.com>
2022-01-05 11:03:55 +0100

    Return response data

--- src/services/index.js
+++ src/services/index.js
@@ -4,31 +4,30 @@ import BASE_URI from '../constants/url';
 
 export const loginService = async (credentials) => {
   const response = await post(`${BASE_URI}v1/users/login`, { credentials });
-  setStorage(response.headers.get('Authorization').split(' ')[1], 'token');
-  return response.json();
+  setStorage(response.headers.authorization.split(' ')[1], 'token');
+  return response.data;
 };
 
 export const registerService = async (credentials) => {
   const response = await post(`${BASE_URI}v1/users`, { credentials });
-  setStorage(response.headers.get('Authorization').split(' ')[1], 'token');
-  return response.json();
+  setStorage(response.headers.authorization.split(' ')[1], 'token');
+  return response.data;
 };
 
 export const getDoctorsService = async () => {
   const response = await get(`${BASE_URI}v1/doctors`);
-  return response.json();
+  return response.data;
 };
 
 export const getDoctorService = async (id) => {
   const response = await get(`${BASE_URI}v1/doctors/${id}`);
-  return response.json();
+  return response.data;
 };
 
 export const addDoctorService = async (doctor) => {
-  const token = getStorage('token');
-  const options = { credentials: doctor, token, picture: true };
+  const options = { credentials: doctor, };
   const response = await post(`${BASE_URI}v1/doctors`, options);
-  return response.json();
+  return response.data;
 };
 
 export const deleteDoctorService = async (id) => {
@@ -36,15 +35,14 @@ export const deleteDoctorService = async (id) => {
 };
 
 export const createAppointmentService = async (appointment) => {
-  const token = getStorage('token');
-  const options = { token, credentials: appointment };
+  const options = { credentials: appointment };
   const response = await post(`${BASE_URI}v1/appointments`, options);
-  return response.json();
+  return response.data;
 };
 
 export const getAppointmentsService = async () => {
   const response = await get(`${BASE_URI}v1/appointments`);
-  return response.json();
+  return response.data;
 };
 
 export const deleteAppointmentService = async (id) => {
