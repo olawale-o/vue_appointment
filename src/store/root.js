fc63ab08c5b2066625c9f8023fa71945705a92a4
olawale-o <omoogunolawale@gmail.com>
2022-04-19 05:58:02 +0100

    Create root store

---
+++ src/store/root.js
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia';
+
+const useRootStore = defineStore('root', {
+  state: () => ({
+    loading: false,
+    error: {
+      message: null,
+    },
+  }),
+  getters: {
+    isLoading: (state) => state.loading,
+    error: (state) => state.error,
+  },
+  actions: {
+    setLoading() {
+      this.loading = !this.loading;
+    },
+    setError(payload) {
+      this.error = payload;
+    }
+  }
+});
+
+export default useRootStore;
