4e53e5e2a651c3c374ba836bfd350bc3578e902f
olawale-o <omoogunolawale@gmail.com>
2022-04-20 03:25:05 +0100

    Use appointment store

--- src/guards/private.js
+++ src/guards/private.js
@@ -1,8 +1,6 @@
 import useAuthUserStore from '@/store/auth';
-// import useRootStore from '../store/root';
 import useDoctorStore from '@/store/doctor';
-// import { actionDoctors, actionDoctorSingle } from '../redux/doctor/action_creators';
-// import { actionAppointments } from '../redux/appointment/action_creators';
+import useAppointmentStore from '@/store/appointment';
 
 export const requiresAuth = (to, from, next) => {
   const store = useAuthUserStore();
@@ -36,15 +34,16 @@ export const requiresDoctors = (to, from, next) => {
 };
 
 export const requiresDoctor = (to, from, next) => {
-  next();
-  // store.dispatch(actionDoctorSingle(to.params.id))
-  //   .then(doctor => {
-  //     to.params.doctor = doctor;
-  //     next();
-  //   });
+  const store = useDoctorStore();
+  store.single(to.params.id)
+    .then(doctor => {
+      to.params.doctor = doctor;
+      next();
+    });
 };
 
 export const requiresAppointment = (to, from, next) => {
-  // store.dispatch(actionAppointments());
+  const store = useAppointmentStore();
+  store.fetchAppointments();
   next();
 };
