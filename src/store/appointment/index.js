9bc1d9994667f661a01742fb12103a65467a174f
olawale-o <omoogunolawale@gmail.com>
2022-04-20 03:24:22 +0100

    Create appointment store

---
+++ src/store/appointment/index.js
@@ -0,0 +1,90 @@
+import { defineStore } from "pinia";
+import {
+  getAppointmentsService,
+  createAppointmentService,
+  deleteAppointmentService,
+} from "@/services";
+
+const useAppointmentStore = defineStore({
+  id: 'appointment',
+  state: () => ({
+    appointment: null,
+    appointments: [],
+    loading: false,
+    error: null,
+  }),
+  getters: {
+    allAppointments: (state) => state.appointments,
+    selectedAppointment: (state) => state.appointment,
+    getAppointmentById: (state) => (id) => state.appointments.find((appointment) => appointment.id === id),
+  },
+  actions: {
+    async fetchAppointments() {
+      this.loading = !this.loading;
+      try {
+        const { data: { appointments } } = await getAppointmentsService();
+        this.updateAppointments(appointments);
+      } catch(e) {
+        this.error = e.message;
+      } finally {
+        this.loading = !this.loading;
+      }
+    },
+
+    async addAppointment(credentials) {
+      this.loading = !this.loading;
+      try {
+        const { data: { appointment } } = await createAppointmentService(credentials);
+        this.updateAppointment(appointment);
+        this.addNewAppointment(appointment);
+      } catch (e) {
+        this.error = e.message;
+      } finally {
+        this.loading = !this.loading;
+      }
+    },
+
+    async delete(credentials){
+      this.loading = !this.loading;
+      try {
+        await deleteAppointmentService(credentials)
+        this.deleteAppointment(credentials);
+      } catch(e) {
+        this.error = e.message;
+      } finally {
+        this.loading = !this.loading;
+      }
+    },
+
+    updateAppointments(payload) {
+      this.appointments = payload;
+    },
+
+    updateAppointment(payload) {
+      this.appointment = payload;
+    },
+
+    addNewAppointment(payload) {
+      this.appointments.concat(payload);
+    },
+
+    deleteAppointment(payload){
+      const { appointment, appointments } = this.$state;
+      if (appointment && appointment.id === payload.id) {
+        this.appointment = null;
+      }
+      this.appointments = appointments.filter((appointment) => appointment.id !== payload);
+    }
+  },
+  persist: {
+    enabled: true,
+    strategies: [
+      {
+        key: 'appointment',
+        storage: localStorage,
+      }
+    ]
+  },
+});
+
+export default useAppointmentStore;
