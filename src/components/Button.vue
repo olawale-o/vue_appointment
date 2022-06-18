b5e134d14a894fa46255d2ae713177c832c9ab15
olawale-o <omoogunolawale@gmail.com>
2022-06-18 23:41:05 +0100

    Create button component

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link :to="href" class="btn btn__large" v-if="href">{{ text }}</router-link>
+  <button type="button" class="btn btn__large" v-else @click="$emit('callBack')">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  emits: ['callBack'],
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    href: {
+      type: String,
+    },
+  },
+  setup() { },
+};
+</script>
